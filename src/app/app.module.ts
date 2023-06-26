ce4dfbd70719f6e19f7afc703eb15e8bcca6f56d
Pertoldi <antoine.pertoldi@gmail.com>
2023-06-26 17:05:08 +0200

    :coffin: remove ng2chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgChartsModule  } from 'ng2-charts';
-import { ChartComponent } from './components/chart/chart.component';
 import { TitleComponent } from './components/title/title.component';
 import { CountryComponent } from './core/pages/country/country.component';
 
@@ -16,7 +15,6 @@ import { CountryComponent } from './core/pages/country/country.component';
     AppComponent,
     HomeComponent,
     NotFoundComponent, 
-    ChartComponent, 
     TitleComponent,
     CountryComponent
   ],
